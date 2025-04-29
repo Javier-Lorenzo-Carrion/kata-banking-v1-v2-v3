844900986d18e1d09620e8e2e6339b7b7caae367
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-29 16:58:35 +0100

    initial commit

---
+++ src/core/banking.ts
@@ -0,0 +1,3 @@
+export function bankingV1() {
+  return 'foo';
+}
---
+++ src/tests/banking.test.ts
@@ -0,0 +1,8 @@
+import { expect } from '@jest/globals';
+import { bankingV1 } from '../core/banking';
+
+describe('Banking version 1 should', () => {
+  it('something', () => {
+    expect(bankingV1()).toBe('');
+  });
+});
